147739fda5d2188074d536820856badae1ae4834
Linnea <lkdamer@gmail.com>
2014-09-03 22:32:11 -0700

    Recommitting because having trouble with the push

--- fav_num.rb
+++ fav_num.rb
@@ -1,4 +1,4 @@
 puts "What is your favorite number?"
 number = gets.to_i
 number += 1
-puts "What about " + number.to_s + "?\nWouldn't that make a bigger and better favorite number?" 
+puts "What about " + number.to_s + "?\nThat is a bigger number. \nWouldn't it make a better favorite number?" 
